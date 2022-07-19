1c529dd6ff88c26ac9219097271df8d399161dce
exerussus <solovyov.production@gmail.com>
2022-07-19 13:35:24 +0300

    Added Mechanics in basic_spell class. Created spell_ruler class

---
+++ mechanics/spell_ruler.py
@@ -0,0 +1,6 @@
+
+
+class SpellRuler:
+
+    def __init__(self):
+        pass
--- spells/basic_spell.py
+++ spells/basic_spell.py
@@ -11,16 +11,30 @@ class BasicSpell:
                  value_health=0,
                  value_psyche=0,
                  value_mana=0,
+                 value_drain_health=0,
+                 value_drain_psyche=0,
+                 value_drain_mana=0,
                  count=0,
+                 stun=0,
+                 attack_stopper=False,
+                 defend_stopper=False,
+
                  ):
 
-        self.spell_id = spell_id          # идентификатор спелла
-        self.name = name                  # Название
-        self.spell_type = spell_type      # Тип спела (атака\защита)
-        self.subtype = subtype            # Подтип спела (защита: абсолютная защита, щит, контрудар; атака: хз, хз, хз)
-        self.direction = direction        # Направление спела (противник, на себя, может быть и оба, но хз пока)
-        self.value_health = value_health  # Количество здоровья
-        self.value_psyche = value_psyche  # Количество психики
-        self.value_mana = value_mana      # Количество энергии
-        self.count = count                # Количество зарядов (если это щит, к примеру), оно же, ходов
+        self.spell_id = spell_id                      # идентификатор спелла
+        self.name = name                              # Название
+        self.spell_type = spell_type                  # Тип спела (атака\защита)
+        self.subtype = subtype                        # Подтип спела (защита: абсолютная защита, щит, контрудар)
+        self.direction = direction                    # Направление спела (противник, на себя, может быть и оба)
+        self.value_health = value_health              # Количество здоровья
+        self.value_psyche = value_psyche              # Количество психики
+        self.value_mana = value_mana                  # Количество энергии
+        self.value_drain_health = value_drain_health  # Похищение здоровья
+        self.value_drain_psyche = value_drain_psyche  # Похищение психики
+        self.value_drain_mana = value_drain_mana      # Похищение энергии
+        self.count = count                            # Количество ходов
+        self.stun = stun                              # Пропуск хода
+        self.attack_stopper = attack_stopper          # Запрещает атаку
+        self.defend_stopper = defend_stopper          # Запрещает защиту
+        
 
