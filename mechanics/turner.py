4ac4eaa0eaa339cf88bd192a48efc0cadf59e43c
exerussus <solovyov.production@gmail.com>
2022-07-21 01:00:02 +0300

    Added spell_adder.py, changed turner.py

--- mechanics/turner.py
+++ mechanics/turner.py
@@ -11,4 +11,12 @@ class Turn:
 
         spells = [spell for spell in self.turn_now.spells]
 
+        if self.turn_now.player:
+            count = 0
+            for spell in spells:
+                count += 1
+                print(f"{count}. {spell} | {spell.value_mana_caster} энергии")  # НЕ ДОЛЖНО РАБОТАТЬ!
+                # Экземпляр спелла ещё не создан, поэтому референс на value_mana_caster должен
+                # вызвать ошибку. Варианты: создание класса при получении спелла, а потом делать
+                # deepcopy при каждом касте, но нужно ещё думать
 
