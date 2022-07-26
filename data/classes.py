06c6ac894391f82c6155b0acd106a9fb2a6235ba
exerussus <solovyov.production@gmail.com>
2022-07-26 16:21:11 +0300

    Added new spells in classes

--- data/classes.py
+++ data/classes.py
@@ -1,9 +1,14 @@
 from spells.fire_hand import FireHand
+from spells.decrease import Decrease
+from spells.energy_absorption import EnergyAbsorption
+from spells.first_shield import FirstShield
+from spells.lightning_discharge import LightningDischarge
 
 classes = {
     1: FireHand,
-    2: FireHand,
-    3: FireHand,
-    4: FireHand,
-    5: FireHand,
+    2: Decrease,
+    3: EnergyAbsorption,
+    4: FirstShield,
+    5: LightningDischarge,
+
 }
